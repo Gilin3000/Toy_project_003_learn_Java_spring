d93f2e8630f208b6bbcafa8ecdd35dfb19039238
Gilin3000 <lotus0110@nate.com>
2023-02-11 00:06:56 +0900

    feat : OrderApp, OrderService

--- core/core/src/main/java/hello/core/order/Order.java
+++ core/core/src/main/java/hello/core/order/Order.java
@@ -1,13 +1,13 @@
 package hello.core.order;
 
-public class order {
+public class Order {
 
     private Long memberId;
     private String itemName;
     private int itemPrice;
     private int discountPrice;
 
-    public order(Long memberId, String itemName, int itemPrice, int discountPrice) {
+    public Order(Long memberId, String itemName, int itemPrice, int discountPrice) {
         this.memberId = memberId;
         this.itemName = itemName;
         this.itemPrice = itemPrice;
