9e650a3f78752f8b472ace1acd156f30d36aa1db
Gilin3000 <lotus0110@nate.com>
2023-02-14 22:22:43 +0900

    feat : PostConstruct, PreDestroy

--- core/core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -3,6 +3,9 @@ package hello.core.lifecycle;
 //import org.springframework.beans.factory.DisposableBean;
 //import org.springframework.beans.factory.InitializingBean;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 //public class NetworkClient implements InitializingBean, DisposableBean {
 public class NetworkClient {
     private String url;
@@ -31,6 +34,7 @@ public class NetworkClient {
 
 //    @Override
 //    public void afterPropertiesSet() throws Exception {
+    @PostConstruct
     public void init() {
         connect();
         call("초기화 연결 메시지");
@@ -38,6 +42,7 @@ public class NetworkClient {
 
 //    @Override
 //    public void destroy() throws Exception {
+    @PreDestroy
     public void close() {
         disconnect();
     }
